80e1d794853b30d654482e3040e3da9397e9d7bb
rodrigojaga <rodrigogalindo3basico@gmail.com>
2023-06-12 17:10:22 -0600

    Segunda Entrega

--- ProyectoCalucladora/app/src/main/java/com/example/proyectocalucladora/MainActivity.java
+++ ProyectoCalucladora/app/src/main/java/com/example/proyectocalucladora/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.proyectocalucladora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
+import com.faendir.rhino_android.AndroidContextFactory;
 import com.google.android.material.button.MaterialButton;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button btReset,btnBorrar;
         Button bt0,bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt9,btnpunto;
-        Button sum,res,mul,div,raiz;
+        Button sum,res,mul,div,raiz,igual;
         tvSalida = findViewById(R.id.salidaNumeros);
         tvResultado = findViewById(R.id.salidaResultado);
         btReset = findViewById(R.id.btn_borrar_todo);
@@ -31,23 +34,54 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         bt0 = findViewById(R.id.btn_0);
-        bt0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                boton(bt0);
-            }
-        });
+        numero(bt0);
         bt1 = findViewById(R.id.btn_1);
-        bt1.setOnClickListener(new View.OnClickListener() {
+        numero(bt1);
+        bt2 = findViewById(R.id.btn_2);
+        numero(bt2);
+        bt3 = findViewById(R.id.btn_3);
+        numero(bt3);
+        bt4 = findViewById(R.id.btn_4);
+        numero(bt4);
+        bt5 = findViewById(R.id.btn_5);
+        numero(bt5);
+        bt6 = findViewById(R.id.btn_6);
+        numero(bt6);
+        bt7 = findViewById(R.id.btn_7);
+        numero(bt7);
+        bt8 = findViewById(R.id.btn_8);
+        numero(bt8);
+        bt9 = findViewById(R.id.btn_9);
+        numero(bt9);
+        btnpunto = findViewById(R.id.btn_punto);
+        numero(btnpunto);
+        sum = findViewById(R.id.btn_suma);
+        numero(sum);
+        res = findViewById(R.id.btn_resta);
+        numero(res);
+        mul = findViewById(R.id.btn_multiplicacion);
+        numero(mul);
+        div = findViewById(R.id.btn_division);
+        numero(div);
+        raiz = findViewById(R.id.btn_raiz);
+        numero(raiz);
+        btnBorrar = findViewById(R.id.btn_borrar_ultimo);
+        btnBorrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                boton(bt1);
+                regresar();
             }
         });
 
-
+        igual = findViewById(R.id.btn_igual);
+        igual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                igualSig();
+            }
+        });
     }
-    public void boton(Button btn){
+    private void boton(Button btn){
 
         String ant = tvSalida.getText().toString();
         String num = btn.getText().toString();
@@ -55,10 +89,58 @@ public class MainActivity extends AppCompatActivity {
         tvSalida.setText(concat);
     }
 
+    private void numero(Button btn){
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boton(btn);
+            }
+        });
+    }
 
+    private void regresar(){
+        String texto = (tvSalida.getText().toString());
+        int lenght = texto.length();
+        if(lenght>0){
+            String a = texto.subSequence(0,lenght-1).toString();
+            tvSalida.setText(a);
+        }
+    }
 
+    public void dataCalculator(View view){
+        Intent siguiente = new Intent(this, parteDatos.class);
+        startActivity(siguiente);
+    }
 
+    private void igualSig(){
+        String expresion = tvSalida.getText().toString();
+        tvResultado.setText(operacion(expresion));
+    }
 
+    private String operacion(String expresion){
+        if(expresion.contains(Character.toString('^'))){
+            String[] partes = expresion.split("\\^");
+            double base = Double.parseDouble(partes[0]);
+            double expo = Double.parseDouble(partes[1]);
+
+            double resultado = Math.pow(base,expo);
+            return String.valueOf(resultado);
+        }else{
+            Context rhino = Context.enter();
+            rhino.setOptimizationLevel(-1);
+            try{
+
+                Scriptable scope = rhino.initStandardObjects();
+                Object result = rhino.evaluateString(scope, expresion, "JavaScript", 1, null);
+                return String.valueOf(result);
+
+            }catch (Exception e){
+
+            }
+        }
+
+        return null;
+    }
 
 
 
--- ProyectoCalucladora/app/src/main/java/com/example/proyectocalucladora/parteDatos.java
+++ ProyectoCalucladora/app/src/main/java/com/example/proyectocalucladora/parteDatos.java
@@ -2,13 +2,197 @@ package com.example.proyectocalucladora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class parteDatos extends AppCompatActivity {
+
+    public static final int decimalPlaces = 10;
+    public static final int decimalPlaces2 = 15;
+    TextView ed,sd;
+    Spinner spinner1, spinnerD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_parte_datos);
+        spinner1 = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.opciones, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter);
+
+        spinnerD = findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.opciones, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerD.setAdapter(adapter);
+
+        ed = findViewById(R.id.entradaData);
+        sd = findViewById(R.id.salidaData);
+        Button limpiar,deletear;
+        Button b0,b1,b2,b3,b4,b5,b6,b7,b8,b9;
+        Button bpunto, biguasl;
+        limpiar = findViewById(R.id.clear);
+        limpiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                regresarData();
+            }
+        });
+
+        deletear = findViewById(R.id.borrarTodo);
+        deletear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ed.setText(""); sd.setText("");
+            }
+        });
+
+        b0 = findViewById(R.id.button0);
+        numeroData(b0);
+        b1 = findViewById(R.id.button1);
+        numeroData(b1);
+        b2 = findViewById(R.id.button2);
+        numeroData(b2);
+        b3 = findViewById(R.id.button3);
+        numeroData(b3);
+        b4 = findViewById(R.id.button4);
+        numeroData(b4);
+        b5 = findViewById(R.id.button5);
+        numeroData(b5);
+        b6 = findViewById(R.id.button6);
+        numeroData(b6);
+        b7 = findViewById(R.id.button7);
+        numeroData(b7);
+        b8 = findViewById(R.id.button8);
+        numeroData(b8);
+        b9 = findViewById(R.id.button9);
+        numeroData(b9);
+        bpunto = findViewById(R.id.buttonPunto);
+        numeroData(bpunto);
+        biguasl = findViewById(R.id.buttonIguaal);
+        biguasl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                botonIgual();
+            }
+        });
+
+
     }
+
+    private void botonData(Button btn){
+
+        String ant = ed.getText().toString();
+        String num = btn.getText().toString();
+        String concat = ant+num;
+        ed.setText(concat);
+    }
+
+    private void numeroData(Button btn){
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                botonData(btn);
+            }
+        });
+    }
+
+    private void botonIgual(){
+        //DecimalFormat dc = new DecimalFormat("#." + "0".repeat(decimalPlaces));
+        //DecimalFormat df = new DecimalFormat("#." + "0".repeat(decimalPlaces));
+        String auxS  = spinner1.getSelectedItem().toString();
+        String auxS2 = spinnerD.getSelectedItem().toString();
+        String ax = ed.getText().toString();
+        switch (auxS){
+            case "Megabytes":
+                switch (auxS2){
+                    case "Megabytes":
+                        sd.setText(ed.getText().toString());
+                        break;
+
+                    case "Gigabytes":
+                        double a = Double.parseDouble(ax);
+                        double result = a/1024;
+                        String b = String.valueOf(result);
+                        sd.setText(b);
+                        break;
+
+                    case "Terabytes":
+                        double a1 = Double.parseDouble(ax);
+                        double result1 = a1/1048576;
+                        String b1 = String.valueOf(result1);
+                        sd.setText(b1);
+                }
+                break;
+
+            case "Gigabytes":
+                switch (auxS2){
+                    case "Megabytes":
+                        double a = Double.parseDouble(ax);
+                        double result = a*1024;
+                        String b = String.valueOf(result);
+                        sd.setText(b);
+                        break;
+
+                    case "Gigabytes":
+                        sd.setText(ed.getText().toString());
+                        break;
+
+                    case "Terabytes":
+                        double a1 = Double.parseDouble(ax);
+                        double result1 = a1/1024;
+                        String b1 = String.valueOf(result1);
+                        sd.setText(b1);
+                        break;
+                }
+                break;
+
+            case "Terabytes":
+                switch (auxS2){
+                    case "Megabytes":
+                        double a = Double.parseDouble(ax);
+                        double result = a*1048576;
+                        String b = String.valueOf(result);
+                        sd.setText(b);
+                        break;
+
+                    case "Gigabytes":
+                        double a1 = Double.parseDouble(ax);
+                        double result1 = a1*1024;
+                        String b1 = String.valueOf(result1);
+                        sd.setText(b1);
+                        break;
+
+                    case "Terabytes":
+                        sd.setText(ed.getText().toString());
+                        break;
+                }
+                break;
+        }
+    }
+
+
+    public void commonCalculator(View view){
+        Intent siguiente = new Intent(this, MainActivity.class);
+        startActivity(siguiente);
+    }
+
+    private void regresarData(){
+        String texto = (ed.getText().toString());
+        int lenght = texto.length();
+        if(lenght>0){
+            String a = texto.subSequence(0,lenght-1).toString();
+            ed.setText(a);
+        }
+    }
+
 }
