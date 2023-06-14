42749d02eefa1a088398bb8bcde9a6fa238a45db
rodrigojaga <rodrigogalindo3basico@gmail.com>
2023-06-14 16:51:28 -0600

    Tercera Entrega

--- ProyectoCalucladora/app/src/main/java/com/example/proyectocalucladora/MainActivity.java
+++ ProyectoCalucladora/app/src/main/java/com/example/proyectocalucladora/MainActivity.java
@@ -12,6 +12,9 @@ import com.google.android.material.button.MaterialButton;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView tvSalida;
@@ -33,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
                 tvResultado.setText("");
             }
         });
+
         bt0 = findViewById(R.id.btn_0);
         numero(bt0);
         bt1 = findViewById(R.id.btn_1);
@@ -80,6 +84,9 @@ public class MainActivity extends AppCompatActivity {
                 igualSig();
             }
         });
+
+
+
     }
     private void boton(Button btn){
 
@@ -113,39 +120,90 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void igualSig(){
+
         String expresion = tvSalida.getText().toString();
         tvResultado.setText(operacion(expresion));
+
     }
 
-    private String operacion(String expresion){
-        if(expresion.contains(Character.toString('^'))){
-            String[] partes = expresion.split("\\^");
-            double base = Double.parseDouble(partes[0]);
-            double expo = Double.parseDouble(partes[1]);
+    private String operacion(String expresion) {
+        int contador = contarSignos(expresion, '^');
+        if (contador > 1) {
+            return operacionPrueba(expresion);
+        } else if (contador == 1) {
+            if (expresion.contains(Character.toString('^'))&&(expresion.contains(Character.toString('+'))||expresion.contains(Character.toString('-'))||expresion.contains(Character.toString('*'))||expresion.contains(Character.toString('/')))){
+                expresion = procesarPotencia(expresion);
+                String resultado = procesado(expresion);
+                return resultado;
+            }else{
+                String[] partes = expresion.split("\\^");
+                String base = partes[0];
+                String expo = partes[1];
+                double v1 = Double.parseDouble(procesado(base));
+                double v2 = Double.parseDouble(procesado(expo));
 
-            double resultado = Math.pow(base,expo);
-            return String.valueOf(resultado);
-        }else{
-            Context rhino = Context.enter();
-            rhino.setOptimizationLevel(-1);
-            try{
-
-                Scriptable scope = rhino.initStandardObjects();
-                Object result = rhino.evaluateString(scope, expresion, "JavaScript", 1, null);
-                return String.valueOf(result);
-
-            }catch (Exception e){
-
+                double resultado = Math.pow(v1, v2);
+                return String.valueOf(resultado);
             }
+
+        } else {
+            return procesado(expresion);
         }
 
+    }
+
+    private String procesado(String expresion) {
+        Context rhino = Context.enter();
+        rhino.setOptimizationLevel(-1);
+        try {
+            Scriptable scope = rhino.initStandardObjects();
+            Object result = rhino.evaluateString(scope, expresion, "JavaScript", 1, null);
+            return String.valueOf(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            Context.exit();
+        }
         return null;
     }
 
+    private String operacionPrueba(String expresion) {
+        ArrayList<String> array = separarCadena(expresion);
+        double result = Double.parseDouble(array.get(0));
+        for (int i = 1; i < array.size(); i++) {
+            result = Math.pow(result, Double.parseDouble(array.get(i)));
+        }
+        return String.valueOf(result);
+    }
 
+    public ArrayList<String> separarCadena(String cadena) {
+        String[] partesArray = cadena.split("\\^");
+        ArrayList<String> partes = new ArrayList<>(Arrays.asList(partesArray));
+        return partes;
+    }
 
+    public int contarSignos(String cadena, char signo) {
+        int contador = 0;
+        for (int i = 0; i < cadena.length(); i++) {
+            if (cadena.charAt(i) == signo) {
+                contador++;
+            }
+        }
+        return contador;
+    }
 
+    private String procesarPotencia(String expresion) {
+        String[] partes = expresion.split("\\^");
+        StringBuilder resultado = new StringBuilder();
+        resultado.append(partes[0]);
 
+        for (int i = 1; i < partes.length; i++) {
+            double v1 = Double.parseDouble(procesado(partes[i]));
+            resultado.append("^").append(v1);
+        }
+
+        return resultado.toString();
+    }
 
 
 
